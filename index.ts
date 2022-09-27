dfbf378d26455e0060dfddcbf550ffb09ee23df2
DowglasSant <dowglas.santana9@gmail.com>
2022-09-26 22:52:58 -0300

    teste vercel

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.post('/', (req: Request, res: Response) => {
+    const { nome, idade, rg } = req.body;
+
+    res.send(nome + " " + idade + " " + rg);
+})
+
+app.listen(3000, () => {
+    console.log(`⚡️[server]: Server is running at port 3000`);
+});
